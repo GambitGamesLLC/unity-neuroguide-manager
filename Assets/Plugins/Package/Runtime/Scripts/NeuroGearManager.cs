14551ab4f2627d5a59a26207d8f1a76209e7eb80
Derrick Barra <gambitgamesllc@gmail.com>
2025-06-06 14:21:56 -0400

    Initial version of script

---
+++ Assets/Plugins/Package/Runtime/Scripts/NeuroGearManager.cs
@@ -0,0 +1,18 @@
+﻿/********************************************************
+ * NeuroGearManager.cs
+ * 
+ * Listens to and responds to events from the NeuroGear hardware
+ * 
+ ********************************************************/
+
+using UnityEngine;
+
+/// <summary>
+/// Singleton Manager for interacting with the NeuroGear hardware
+/// </summary>
+public class NeuroGearManager : MonoBehaviour
+{
+    
+	
+	
+} //END NeuroGearManager
